af696039d392ae0ab133e7bc7f40ecc94c129273
Shoham Shitrit <sshitrit@microsoft.com>
2023-10-14 23:35:45 -0400

    initial site changes

---
+++ src/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+      <p>Here you can find information about me and my work.</p>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/ResumePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ResumePage() {
+  return (
+    <div>
+      <h1>My Resume</h1>
+      <p>Here you can find my resume and work experience.</p>
+    </div>
+  );
+}
+
+export default ResumePage;
